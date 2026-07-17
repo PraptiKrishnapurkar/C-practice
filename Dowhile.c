9076b50fd46dd738e3372b84a529254fb1743849
Praptii <praptikrishnapurkar01@gmail.com>
2026-07-17 23:19:56 +0530

    Add files via upload

---
+++ Dowhile.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+main(){
+    int i=1;
+    do
+    {
+        printf("\nHELOO");
+        i++;
+
+    }
+    while(i<=10);
+    //Exit control loop.
+    //loop-9
+    //condition=10
+}
