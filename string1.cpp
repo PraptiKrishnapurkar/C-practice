475838ea355ca88b6cbe89fa3ddbe64c98b26223
Praptii <praptikrishnapurkar01@gmail.com>
2026-09-15 23:43:58 +0530

    Add files via upload

---
+++ string1.cpp
@@ -0,0 +1,13 @@
+
+#include<string.h>
+#include<iostream>
+using namespace std;
+int main()
+{
+    int l;
+    string str1;
+    cout<<"Enter your name: ";
+    getline (cin,str1);
+    cout<<str1; 
+    
+}
